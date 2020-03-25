33020cef4e903323f9055b8c20e754ad2446d75c
mostafa-rasheed <mstafarasheed@gmail.com>
2020-03-25 07:38:26 +0200

    Dealing with the database

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
-Route::get('/',function () {
-    
-   
-     return view('welcome');
 
-});
 Route::get('/','TaskController@index');
+
 Route::get('task/{id}', 'TaskController@show');
+
+Route::post('store','TaskController@store');
